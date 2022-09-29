ffb2ffc366b86d750525588aa29cdc1f609621b9
Thomas O'Donnell <Thomas.e.odonnell@gmail.com>
2022-09-29 11:00:29 +0100

    Update build_script.sh
    - Added ASCII Art

--- build_script.sh
+++ build_script.sh
@@ -1,79 +1,60 @@
 #!/bin/bash
 
+echo "CiAgXyAgICBfICAgICAgICAgICBfICAgICAgIF8gICAgICAgICAgX19fX18gICAgICAgICAgIF8gICAgICAgXyAgIAogfCB8ICB8IHwgICAgICAgICB8IHwgICAgIHwgfCAgICAgICAgLyBfX19ffCAgICAgICAgIChfKSAgICAgfCB8ICAKIHwgfCAgfCB8XyBfXyAgIF9ffCB8IF9fIF98IHxfIF9fXyAgfCAoX19fICAgX19fIF8gX18gXyBfIF9fIHwgfF8gCiB8IHwgIHwgfCAnXyBcIC8gX2AgfC8gX2AgfCBfXy8gXyBcICBcX19fIFwgLyBfX3wgJ19ffCB8ICdfIFx8IF9ffAogfCB8X198IHwgfF8pIHwgKF98IHwgKF98IHwgfHwgIF9fLyAgX19fXykgfCAoX198IHwgIHwgfCB8XykgfCB8XyAKICBcX19fXy98IC5fXy8gXF9fLF98XF9fLF98XF9fXF9fX3wgfF9fX19fLyBcX19ffF98ICB8X3wgLl9fLyBcX198CiAgICAgICAgfCB8ICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICB8IHwgICAgICAgIAogICAgICAgIHxffCAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgfF98ICAgICAgICAK" | base64 -d
+
+shopt -s nocasematch
+
 #Checking if script is running at root.
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-#Update repo and upgrade system first.
+#cating source list to ensure kali sources are within the correct repo
+cat /etc/apt/sources.list
 
-# Update
-echo "=================================="
-echo "Update"
-echo "=================================="
-sudo apt-get update
-
-
-# Upgrade
-echo "=================================="
-echo "Upgrade"
-echo "=================================="
-sudo apt-get upgrade
-
-
-# Distribution Upgrade
-echo "=================================="
-echo "Distribution Upgrade"
-echo "=================================="
-sudo apt-get dist-upgrade
-
-#The following commands will install common tools used within my personal kali install. More will be updated as I progress (2022)
-#Install common global tools please feel free to add or remove tools from here this is personal preference
-apt-get install htop iftop python3-pip python2 seclists terminator -y 
-
-#create new tools directory and report directory for kali on first boot
-mkdir /opt/tools
-mkdir /home/kali/Documents/Reports
-mkdir /home/kali/Documents/Reports/Reports_in_progress
-
-
-#cd into new directory 
-cd /opt/tools
-
-#Enum4linux-ng
-apt-get install smbclient python3-ldap3 python3-yaml python3-impacket -y
-git clone https://github.com/cddmp/enum4linux-ng.git
-
-#testssl
-git clone --depth 1 https://github.com/drwetter/testssl.sh.git
-
-#Powersploit 
-git clone https://github.com/PowerShellMafia/PowerSploit.git
- 
-#Impacket
-git clone https://github.com/CoreSecurity/impacket.git
-python3 opt/tools/impacket/setup.py install
- 
-#Winpeas and Linpeas
-git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git
-
-#Linenum 
-git clone https://github.com/rebootuser/LinEnum.git
-
-#Responder
-git clone https://github.com/lgandx/Responder.git
-
-#dnscan
-git clone https://github.com/rbsec/dnscan.git
-pip3 install -r opt/tools/dnscan/requirements.txt
-
-#winrm
-gem install evil-winrm
-
-#wifite (For wireless testing)
-git clone https://github.com/kimocoder/wifite2.git 
-pip3 install -r opt/tools/wifite2/requirements.txt
-
-
-
+#Checking if you want to update repos
+echo "Do you want to update repos continue?(Y/N)"
+read input
+if [[ "{$input}" == *"y"* ]]
+then
+apt update 
+fi
+if [[ "{$input}" == *"n"* ]]
+then exit 1
+echo "continue"
+fi
+#Checking if you want to upgrade packages
+echo "Do you want to upgrade packages continue?(Y/N)"
+read input
+if [[ "{$input}" == *"y"* ]]
+then
+apt -y upgrade
+fi
+if [[ "{$input}" == *"n"* ]]
+then exit 1
+echo "continue"
+fi
+#Checking if you want to upgrade the distribution?
+echo "Do you want to upgrade packages continue?(Y/N)"
+read input
+if [[ "{$input}" == *"y"* ]]
+then
+apt -y dist-upgrade
+fi
+if [[ "{$input}" == *"n"* ]]
+then exit 1
+echo "continue"
+fi
+#checking if you want to remove packages
+echo "Do you want to auto remove packages?(Y/N)"
+read input
+if [[ "{$input}" == *"y"* ]]
+then
+apt-get -y autoremove
+fi
+if [[ "{$input}" == *"n"* ]]
+then exit 1
+echo "continue"
+fi
+done
