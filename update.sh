15e2a7c65f638067c9049b37b9fc8611d0ef1c7f
Thomas O'Donnell <Thomas.e.odonnell@gmail.com>
2022-10-07 15:03:06 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,58 +1,102 @@
 #!/bin/bash
 
-shopt -s nocasematch
-
 #Checking if script is running at root.
 if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+  then echo "[!] Please run as root"
   exit
 fi
 
-#cating source list to ensure kali sources are within the correct repo
-cat /etc/apt/sources.list
+banner(){
+echo "CiAgIF9fICBfXyAgICAgICAgICBfXyAgICAgIF9fICAgICAgICAgIF9fX19fICAgICAgICAgICBfICAgICAgIF9fIAogIC8gLyAvIC9fX18gIF9fX18vIC9fX18gXy8gL19fX18gICAgIC8gX19fL19fX19fX19fX18oXylfX18gIC8gL18KIC8gLyAvIC8gX18gXC8gX18gIC8gX18gYC8gX18vIF8gXCAgICBcX18gXC8gX19fLyBfX18vIC8gX18gXC8gX18vCi8gL18vIC8gL18vIC8gL18vIC8gL18vIC8gL18vICBfXy8gICBfX18vIC8gL19fLyAvICAvIC8gL18vIC8gL18gIApcX19fXy8gLl9fXy9cX18sXy9cX18sXy9cX18vXF9fXy8gICAvX19fXy9cX19fL18vICAvXy8gLl9fXy9cX18vICAKICAgIC9fLyAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIC9fLyAgICAgICAgICAgCg==" | base64 -d
+}
 
-#Checking if you want to update repos
-echo "Do you want to update repos continue?(Y/N)"
-read input
-if [[ "{$input}" == *"y"* ]]
-then
-apt update 
-fi
-if [[ "{$input}" == *"n"* ]]
-then exit 1
-echo "continue"
-fi
-#Checking if you want to upgrade packages
-echo "Do you want to upgrade packages continue?(Y/N)"
-read input
-if [[ "{$input}" == *"y"* ]]
-then
-apt -y upgrade
-fi
-if [[ "{$input}" == *"n"* ]]
-then exit 1
-echo "continue"
-fi
-#Checking if you want to upgrade the distribution?
-echo "Do you want to upgrade packages continue?(Y/N)"
-read input
-if [[ "{$input}" == *"y"* ]]
-then
-apt -y dist-upgrade
-fi
-if [[ "{$input}" == *"n"* ]]
-then exit 1
-echo "continue"
-fi
-#checking if you want to remove packages
-echo "Do you want to auto remove packages?(Y/N)"
-read input
-if [[ "{$input}" == *"y"* ]]
-then
-apt-get -y autoremove
-fi
-if [[ "{$input}" == *"n"* ]]
-then exit 1
-echo "continue"
-fi
-done
+update-repo(){
+	echo "[+] Updating Repos"
+	apt update > /dev/null 2>&1
+}
+
+update-package(){
+	echo "[+] Updating Packages"
+	apt -y upgrade > /dev/null 2>&1
+}
+
+update-distro(){
+	echo "[+] Updating Distribution"
+	apt -y dist-upgrade > /dev/null 2>&1
+}
+
+remove-packages(){
+	echo "[+] Removing Old Packages"
+	apt-get -y autoremove > /dev/null 2>&1
+}
+
+repo-sources(){
+#cating source list to ensure kali sources are within the correct repo
+echo "[!] Repo Sources"
+cat /etc/apt/sources.list
+}
+
+# Display Banner
+banner
+
+# Set case-insensitivity
+shopt -s nocasematch
+
+#Display Repo Sources
+#repo-sources
+
+# Checking For Unattended Mode
+case "$1" in
+-u)
+	echo "[!] Unattended Mode"
+	update-repo
+	update-package
+	update-distro
+	remove-packages
+	;;
+*)
+	echo "[!] Attended Mode - use the '-u' argument for unattended mode"
+	#Checking if you want to update repos
+	echo "[?] Do you want to update repos continue?(Y/N)"
+	read input
+	if [[ "{$input}" == *"n"* ]]; then 
+		continue
+	elif [[ "{$input}" == *"y"* ]]; then
+		update-repo
+	fi
+
+
+	#Checking if you want to upgrade packages
+	echo "[?] Do you want to update repos continue?(Y/N)"
+	read input
+	if [[ "{$input}" == *"n"* ]]; then 
+		continue
+	elif [[ "{$input}" == *"y"* ]]; then
+		update-package
+	fi
+
+
+	#Checking if you want to upgrade distro
+	echo "[?] Do you want to update repos continue?(Y/N)"
+	read input
+	if [[ "{$input}" == *"n"* ]]; then 
+		continue
+	elif [[ "{$input}" == *"y"* ]]; then
+		update-distro
+	fi
+
+
+	#checking if you want to remove packages
+	echo "[?] Do you want to remove old packages?(Y/N)"
+	read input
+	if [[ "{$input}" == *"n"* ]]; then 
+		continue
+	elif [[ "{$input}" == *"y"* ]]; then
+		remove-packages
+	fi
+  ;;
+esac
+
+
+echo "[!] Update Finished!"
+
